bd765b93cecee5f31c554c552547869051ca90a6
Murat <73017654+Texnoloq@users.noreply.github.com>
2021-10-28 15:37:15 +0400

    Sağ click əngəlləmə

---
+++ rightclickblock.js
@@ -0,0 +1,37 @@
+// Bildirişli bloklama | Aşağıdaki mətni özəlləşdirə bilərsiniz
+
+function IE(e) {
+     if (navigator.appName == "Microsoft Internet Explorer" && (event.button == "2" || event.button == "3")) {
+          alert('UYARI BEYBI');
+          return false;
+     }
+}
+function NS(e) {
+     if (document.layers || (document.getElementById && !document.all)) {
+          if (e.which == "2" || e.which == "3") {
+               alert(' Bura mesajınızı yazın');
+               return false;
+          }
+     }
+}
+document.onmousedown=IE;document.onmouseup=NS;document.oncontextmenu=new Function("return false");
+
+
+
+// Bildirişsiz bloklama
+
+function IE(e) {
+     if (navigator.appName == "Microsoft Internet Explorer" && (event.button == "2" || event.button == "3")) {
+          alert('UYARI BEYBI');
+          return false;
+     }
+}
+function NS(e) {
+     if (document.layers || (document.getElementById && !document.all)) {
+          if (e.which == "2" || e.which == "3");
+               return false; 
+     }
+}
+document.onmousedown=IE;document.onmouseup=NS;document.oncontextmenu=new Function("return false");
+//-->
+
